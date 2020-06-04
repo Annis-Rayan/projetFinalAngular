0562efe500a81a3cbf3eccb055e686bb63e59c23
Annis-Rayan <annisr.b@gmail.com>
2020-06-04 20:14:19 +0200

    modif single user

--- src/app/component/single-user/single-user.component.ts
+++ src/app/component/single-user/single-user.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Lieu} from '../model/lieu';
 import {LieuService} from '../../services/lieu.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {User} from '../model/user';
 import {UserService} from '../../services/user.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-single-user',
@@ -12,11 +13,13 @@ import {UserService} from '../../services/user.service';
 })
 export class SingleUserComponent implements OnInit {
 
-
   private _user: User = new User();
+  private _users: User[] = [];
   private _id: number;
   private _erreur: boolean = false;
 
+  private _base64Data: any;
+  private _convertedImage: any;
 
   ngOnInit(): void {
     this._erreur = false;
@@ -31,13 +34,26 @@ export class SingleUserComponent implements OnInit {
         );
       }
     });
-
+    this.users[0] = this.user;
+    console.log(this.users);
   }
 
 
-
   constructor(private userService: UserService,
-              private activatedRoute: ActivatedRoute, private router: Router) { }
+              private activatedRoute: ActivatedRoute, private router: Router) {
+  }
+
+
+  public getImage(user: User): Observable<string> {
+    if (user.imageProfil != null) {
+      this._base64Data = user.imageProfil.pic;
+      this._convertedImage = 'data:image/jpeg;base64,' + this._base64Data;
+    }
+
+    return new Observable(value => {
+      value.next(this._convertedImage);
+    });
+  }
 
 
   get user(): User {
@@ -46,6 +62,30 @@ export class SingleUserComponent implements OnInit {
 
   set user(value: User) {
     this._user = value;
+  }
+
+  get users(): User[] {
+    return this._users;
+  }
+
+  set users(value: User[]) {
+    this._users = value;
+  }
+
+  get base64Data(): any {
+    return this._base64Data;
+  }
+
+  set base64Data(value: any) {
+    this._base64Data = value;
+  }
+
+  get convertedImage(): any {
+    return this._convertedImage;
+  }
+
+  set convertedImage(value: any) {
+    this._convertedImage = value;
   }
 
   get id(): number {
